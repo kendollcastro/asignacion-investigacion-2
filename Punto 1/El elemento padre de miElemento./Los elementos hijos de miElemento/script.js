2c7e80021499dfa09b9724e8532be5cd5fee5ebf
kendollcastro <kendollcastro@gmail.com>
2020-08-13 12:02:52 -0600

    Agregamos la propieda lastChild en el archivo

--- Punto 1/El elemento padre de miElemento./Los elementos hijos de miElemento/script.js
+++ Punto 1/El elemento padre de miElemento./Los elementos hijos de miElemento/script.js
@@ -1,2 +1,5 @@
 //Parece, al menos intuitivamente, que document.getElementById('contenido').firstChild debería seleccionarse el primer párrafo. Sin embargo, sólo ocurre así en Internet Explorer.
 document.getElementById('contenido').firstChild 
+
+//La propiedad lastChild funciona exactamente como firstChild, pero se refiere el último de los hijos de un elemento. Se aplican, por tanto, las mismas indicaciones anteriores.
+document.getElementById('contenido').lastChild 
