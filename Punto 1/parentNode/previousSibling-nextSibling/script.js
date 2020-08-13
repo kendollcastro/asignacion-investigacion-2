657bde8ae897eacf6ff7668f621830341b09fb38
kendollcastro <kendollcastro@gmail.com>
2020-08-13 12:33:44 -0600

    Agregamos el codigo al repositorio

--- Punto 1/parentNode/previousSibling-nextSibling/script.js
+++ Punto 1/parentNode/previousSibling-nextSibling/script.js
@@ -1,3 +1,8 @@
-
+//haciendo referencia a él como el hermano anterior del elemento strong:
 
 document.getElementById('otro-parrafo').getElementsByTagName('strong')[0].previousSibling;
+
+//Ahora seleccionaremos el hermano posterior del elemento em:
+
+document.getElementById('otro-parrafo').getElementsByTagName('em')[0].nextSibling;
+
